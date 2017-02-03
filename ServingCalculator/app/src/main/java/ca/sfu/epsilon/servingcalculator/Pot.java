403c8c1e232a370206e693dce3660c541bf4cd93
PaulWAllan <allanpaulw@gmail.com>
2017-02-02 20:27:29 -0800

    Created a brand new, vanilla project. This will hopefully work for you Kenney. Also added the Pot and Pot Collection files. they throw errors, due to still being skeletons, so please ignore those.

---
+++ ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/Pot.java
@@ -0,0 +1,34 @@
+package your.package.here;
+
+/**
+ * Store information about a single pot
+ */
+
+public class Pot {
+
+    // Set member data based on parameters.
+    public Pot(String name, int weightInG) {
+
+    return;
+    }
+
+    // Return the weight
+    public int getWeightInG() {
+        return this.weight;
+    }
+
+    // Set the weight. Throws IllegalArgumentException if weight is less than 0.
+    public void setWeightInG(int weightInG) {
+    }
+
+    // Return the name.
+    public String getName() {
+        return this.name;
+    }
+
+    // Set the name. Throws IllegalArgumentException if name is an empty string (length 0),
+    // or if name is a null-reference.
+    public void setName(String name) {
+
+    }
+}
