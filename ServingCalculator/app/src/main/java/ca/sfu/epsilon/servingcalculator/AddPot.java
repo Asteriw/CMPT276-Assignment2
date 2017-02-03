8f3f00165115fede5c5a104fc48520a1f3ce2a73
Kenney <kenneyw@sfu.ca>
2017-02-03 15:18:22 -0800

    Added lines to AddPot and Mainactivity. Also Paul, add hints to yo thangs.

--- ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/AddPot.java
+++ ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/AddPot.java
@@ -1,7 +1,11 @@
 package ca.sfu.epsilon.servingcalculator;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddPot extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class AddPot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pot);
+
+        setupEndActivityButton();
+    }
+
+    private void setupEndActivityButton() {
+        Button endAddPot = (Button) findViewById(R.id.btn_end_addpot);
+        endAddPot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, AddPot.class);
     }
 }
--- ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/MainActivity.java
+++ ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/MainActivity.java
@@ -1,7 +1,11 @@
 package ca.sfu.epsilon.servingcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAddPotLaunch();
+    }
+
+    private void setupAddPotLaunch() {
+        Button startAddPot = (Button) findViewById(R.id.btn_add_pot);
+        startAddPot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked Add Pot", Toast.LENGTH_SHORT)
+                        .show();
+
+                Intent AddPotintent = AddPot.makeIntent(MainActivity.this);
+                startActivity(AddPotintent);
+            }
+        });
     }
 }
