8b3e97abcf529faf830c386b9abdc5eb1620bbc5
PaulWAllan <allanpaulw@gmail.com>
2017-02-03 11:45:16 -0800

    Pushing 2 new classes that will end up being activities for the other screens

---
+++ ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/EditPot.java
@@ -0,0 +1,13 @@
+package ca.sfu.epsilon.servingcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
