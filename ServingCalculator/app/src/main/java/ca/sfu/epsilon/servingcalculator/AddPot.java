3ce9a9c8f813469f4cc143ebad3d4254c0d92e8c
PaulWAllan <allanpaulw@gmail.com>
2017-02-04 17:32:21 -0800

    App crashing when I click add pot. there is a commented line in addpot that if uncommented crashes. looking for fix.

--- ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/AddPot.java
+++ ServingCalculator/app/src/main/java/ca/sfu/epsilon/servingcalculator/AddPot.java
@@ -4,18 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import ca.sfu.epsilon.servingcalculator.Pot;
 
 public class AddPot extends AppCompatActivity {
+
+    int weight;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pot);
-
+        setupNameEditText();
+        setupWeightEditText();
         setupEndActivityButton();
+        setupAddPotButton();
+    }
+
+    private void setupNameEditText() {
+        EditText editText = (EditText) findViewById(R.id.Pot_Name_Field);
+        String name = editText.getText().toString();
+    }
+
+    private void setupWeightEditText() {
+        EditText editText = (EditText) findViewById(R.id.Pot_Weight_Field);
+        //int weight = Integer.valueOf(editText.getText().toString());
     }
 
     private void setupEndActivityButton(){
@@ -33,12 +51,14 @@ public class AddPot extends AppCompatActivity {
         acceptAddPot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makePot(String.valueOf(R.id.Pot_Name_Field), R.id.Pot_Weight_Field);
+
+                makePot(name, weight);
             }
 
             private void makePot(String name, int weight) {
-                if (!name.equals(null) && weight > 0) {
+                if ((!name.equals(null)) && weight > 0) {
                     Pot pot = new Pot(name, weight);
+                    Log.i("CalculateServing", "Created new pot with name " + name + " and weight " + weight);
                 } else {
                     throw new IllegalArgumentException("Error: Invalid Pot Parameters.");
                 }
